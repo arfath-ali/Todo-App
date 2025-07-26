a4fe8901a53d8142879eb9ddd376c253ffba9aab
arfath-ali <1290sfaa28@gmail.com>
2025-07-26 18:37:24 +0530

    update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://todo-app-backend-w88c.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
